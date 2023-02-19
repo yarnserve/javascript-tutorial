68f5ea511b7df8609910ced03535e32897c8b96d
yarnserve <yarnserve@gmail.com>
2023-02-19 14:57:19 +0900

    js

--- tutorial/07_email/app.js
+++ tutorial/07_email/app.js
@@ -1,16 +1,18 @@
-window.onload = function () {
-  document
-    .getElementById('contact-form')
-    .addEventListener('submit', function (event) {
-      event.preventDefault()
-      this.contact_number.value = (Math.random() * 100000) | 0
-      emailjs.sendForm('service_3g7hac5', 'template_cpzziah', this).then(
-        function () {
-          console.log('SUCCESS!')
-        },
-        function (error) {
-          console.log('FAILED...', error)
-        }
-      )
-    })
+const form = document.querySelector('#contact-form')
+
+function sendEmail() {
+  form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    this.contact_number.value = (Math.random() * 100000) | 0
+    emailjs.sendForm('service_3g7hac5', 'template_cpzziah', this).then(
+      function () {
+        console.log('SUCCESS!')
+      },
+      function (error) {
+        console.log('FAILED...', error)
+      }
+    )
+  })
 }
+
+sendEmail()
