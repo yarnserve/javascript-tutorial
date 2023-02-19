b065e22c574a00bb4ba07e81fbda37296673117b
yarnserve <yarnserve@gmail.com>
2023-02-19 15:23:29 +0900

    js

--- tutorial/07_email/app.js
+++ tutorial/07_email/app.js
@@ -1,12 +1,18 @@
 const form = document.querySelector('#contact-form')
+const dimm = document.querySelector('.dimm')
+const modal = document.querySelector('.modal')
 
 function sendEmail() {
   form.addEventListener('submit', function (event) {
     event.preventDefault()
     this.contact_number.value = (Math.random() * 100000) | 0
+
+    dimm.classList.add('active')
+
     emailjs.sendForm('service_3g7hac5', 'template_cpzziah', this).then(
       function () {
         console.log('SUCCESS!')
+        modal.classList.add('active')
       },
       function (error) {
         console.log('FAILED...', error)
