eac21963e99b734bf3f858feba4d1509ea5506ec
yarnserve <yarnserve@gmail.com>
2023-02-19 09:48:29 +0900

    js

--- tutorial/03_modal/app2.js
+++ tutorial/03_modal/app2.js
@@ -1,1 +1,10 @@
-console.log('fadsfaksfdjaksld')
+const content = document.querySelector('.content')
+const modal = document.querySelector('.modal')
+const btns = document.querySelectorAll('.btn')
+
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function () {
+    content.classList.toggle('active')
+    modal.classList.toggle('active')
+  })
+})
